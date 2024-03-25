93f8c46fdadb869bcba5f2c3903889dc1b49e7cf
mj-end <mj.sharifimanesh@gmail.com>
2024-03-25 17:05:37 +0330

    Creating About

---
+++ src/Pages/AboutUs/TeamMember/index.jsx
@@ -0,0 +1,111 @@
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Divider,
+  Grid,
+  Typography,
+} from "@mui/material";
+import React, { useEffect, useRef} from "react";
+
+export default function TeamMember({ img, name, skill }) {
+  const cardTitle = useRef();
+  const cardInfo = useRef();
+  const cardImage = useRef();
+  useEffect(() => {
+    cardTitle.current.addEventListener("mouseover",()=>{
+        cardImage.current.style.height="100px"
+        cardInfo.current.style.top="100px"
+        cardInfo.current.style.visibility="visible"
+        cardInfo.current.style.opacity="1"
+        cardTitle.current.style.visibility="hidden"
+        cardTitle.current.style.opacity="0"
+
+    })
+    cardInfo.current.addEventListener("mouseleave",()=>{
+        cardImage.current.style.height="270px"
+        cardInfo.current.style.top="270px"
+        cardInfo.current.style.visibility="hidden"
+        cardInfo.current.style.opacity="0"
+        cardTitle.current.style.visibility="visible"
+        cardTitle.current.style.opacity="1"
+
+    })
+  }, []);
+
+  return (
+    <Grid item xs={10} sm={6} md={4}>
+      <Card sx={{ height: "350px", position: "relative" }}>
+        <CardMedia
+          ref={cardImage}
+          component={"img"}
+          src={`assets/images/about-us/${img}`}
+          alt={name}
+          sx={{ height: "270px", objectPosition: "top", transition: "0.3s" }}
+        />
+        <CardContent ref={cardTitle} sx={{ height: "80px" }}>
+          <Typography variant="m" component={"h2"} textAlign={"center"}>
+            {name}
+          </Typography>
+        </CardContent>
+        <CardContent
+          ref={cardInfo}
+          sx={{
+            height: "250px",
+            width:"100%",
+            position: "absolute",
+            top: "270px",
+            left:"0",
+            visibility: "hidden",
+            opacity: "0",
+            zIndex: "10",
+            transition: "0.3s",
+            display:"flex",
+            flexDirection:'column',
+            alignItems:"center"
+          }}
+        >
+          <Typography variant="m" component={"h2"} textAlign={"center"}>
+            {name}
+          </Typography>
+          <Typography variant="s" sx={{ opacity: "0.85" }} textAlign={"center"}>
+            {skill}
+          </Typography>
+          <Divider
+            sx={{
+              marginX: "auto",
+              width: "40px",
+              height: "5px",
+              backgroundColor: "#FFB51F",
+              marginTop: "25px",
+              marginBottom: "15px",
+            }}
+          />
+          <Typography
+            variant="s"
+            sx={{ opacity: "0.85" }}
+            textAlign={"center"}
+            gutterBottom
+          >
+            Follow him on:
+          </Typography>
+          <Grid container spacing={4} sx={{paddingX:{xs:"80px",md:"40px"} }}>
+            <Grid item xs={4}>
+              <img src={`assets/images/footer/linkedin.svg`} />
+            </Grid>
+            <Grid item xs={4}>
+              <img src={`assets/images/footer/dribbble.svg`} />
+            </Grid>
+            <Grid item xs={4}>
+              <img src={`assets/images/footer/twitter.svg`} />
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+}
+{
+  /* <Box><img src={`assets/images/about-us${img}`} alt={name}/></Box>
+<Box><Typography variant='s' component={"h2"}>{name}</Typography></Box> */
+}
---
+++ src/Pages/AboutUs/index.jsx
@@ -0,0 +1,243 @@
+import React, { useState,useEffect } from "react";
+import Hero from "../../Components/Hero";
+import { useLocation } from "react-router-dom";
+import {
+  Box,
+  Grid,
+  Container,
+  Stack,
+  Input,
+  InputLabel,
+  Typography,
+  Divider,
+  Button,
+} from "@mui/material";
+import TeamMember from "./TeamMember";
+import { Send } from "@mui/icons-material";
+
+export default function AboutUs() {
+  const [teamMembers,setTeamMembers] = useState()
+  useEffect(()=>{
+    try {
+      (async()=>{
+        const res = await fetch("http://localhost:3000/teamMembers")
+        const data = await res.json()
+        setTeamMembers(data)
+      })()
+    } catch (error) {
+      console.log(error)
+    }
+  },[])
+  const pathName = useLocation();
+  const page = pathName.pathname;
+  return (
+    <>
+      <Hero
+        img={"about-us/about-us-hero.jpg"}
+        title={"About Us"}
+        subtitle={"Nulla vitae elit libero, a pharetra augue mollis interdum."}
+      />
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          gap: { xs: "60px", sm: "80px", md: "150px" },
+          marginBottom: { xs: "60px", sm: "80px", md: "150px" },
+        }}
+      >
+        <Stack
+          direction={{ xs: "column", sm: "row" }}
+          columnGap={{ sm: 8, md: 16, lg: 24 }}
+          rowGap={4}
+        >
+          <Box
+            sx={{
+              borderLeft: "5px solid #FFB51F",
+              height: "85px",
+              display: "flex",
+              alignItems: "center",
+              paddingLeft: "15px",
+            }}
+          >
+            <Typography variant="m" component="h4">
+              Our <br /> History
+            </Typography>
+          </Box>
+          <Box>
+            <Typography variant="s" component={"p"}>
+              <span style={{ fontSize: "32px", fontWeight: "600" }}>M</span>
+              aecenas faucibus neque nec purus viverra molestie. Sed euismod
+              eleifend faucibus. Maecenas viverra massa quis felis finibus
+              posuere. Cras ut luctus quam. Vestibulum eget lectus id nulla
+              tincidunt posuere. Cras eget vehicula lorem, vel posuere mauris.
+              Etiam sollicitudin elit eget odio sollicitudin posuere. Vivamus
+              quis lorem sit amet eros consequat egestas eu non lorem. Proin
+              convallis rhoncus urna. Fusce vehicula placerat ultrices. Nam id
+              ultricies risus, vitae auctor urna. Curabitur augue felis, semper
+              quis neque auctor, ornare mattis lacus.
+            </Typography>
+          </Box>
+        </Stack>
+        <Stack
+          direction={{ xs: "column", md: "row-reverse" }}
+          columnGap={{ sm: 6, md: 12, lg: 16 }}
+          rowGap={4}
+        >
+          <Box
+            sx={{
+              borderRight: "5px solid #FFB51F",
+              height: "85px",
+              display: "flex",
+              flexDirection: "row-reverse",
+              alignItems: "center",
+              paddingRight: "15px",
+            }}
+          >
+            <Typography variant="m" component="h4">
+              Our <br /> Team
+            </Typography>
+          </Box>
+          <Grid container spacing={2} justifyContent={"center"}>
+            {teamMembers?.map((e, index) => (
+              <TeamMember
+                key={index}
+                img={e.img}
+                name={e.name}
+                skill={e.skill}
+              />
+            ))}
+          </Grid>
+        </Stack>
+      </Container>
+      <Box
+        sx={{
+          backgroundColor: "black",
+          width: "100%",
+          height: { xs: "200px", md: "340px" },
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-around",
+          alignItems: "center",
+          paddingY: { xs: "15px", md: "30px" },
+        }}
+      >
+        <img src="assets/images/about-us/magic.svg" alt="magic" />
+        <Typography variant="m" sx={{ color: "white" }}>
+          " Lorem ipsum doler sit amet "
+        </Typography>
+        <Divider
+          sx={{
+            marginX: "auto",
+            width: "40px",
+            height: "5px",
+            backgroundColor: "#FFB51F",
+          }}
+        />
+      </Box>
+
+      <Box sx={{ position: "relative", height: "390px" }}>
+        <Box
+          sx={{
+            borderLeft: "5px solid #FFB51F",
+            height: "85px",
+            display: "flex",
+            alignItems: "center",
+            paddingLeft: "15px",
+            position: "absolute",
+            top: "25%",
+            left: "5%",
+          }}
+        >
+          <Typography variant="m" component="h4">
+            Our <br /> location
+          </Typography>
+        </Box>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d25714.177790868052!2d59.5767962!3d36.3299517!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2s!4v1707208614906!5m2!1sen!2s"
+          style={{ border: "0", width: "100%", height: "100%" }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+          title="Our Location"
+        ></iframe>
+      </Box>
+      <Stack
+        direction={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        gap={4}
+        height={"380px"}
+        bgcolor={"primary.main"}
+      >
+        <Box>
+          <Typography variant="l" gutterBottom>
+            JOIN OUR NEWSLETTER
+          </Typography>
+          <Divider
+            sx={{
+              marginX: "auto",
+              width: "40px",
+              height: "5px",
+              backgroundColor: "white",
+            }}
+          />
+        </Box>
+        <Stack
+          direction={{ xs: "column", sm: "row" }}
+          alignItems={{ xs: "center", md: "end" }}
+          justifyContent={"center"}
+          columnGap={6}
+          rowGap={4}
+          width="100%"
+        >
+          <Box>
+            <InputLabel htmlFor="subscription-email"></InputLabel>
+            <Input
+              id="subscription-email"
+              sx={{
+                transition: "all 0.3s",
+                height: "50px",
+                "&:hover:not(.Mui-disabled, .Mui-error):before": {
+                  border: "2px solid #00000080",
+                },
+                "&::before": {
+                  top: "0",
+                  border: "1px solid #00000080",
+                  borderRadius: "5px",
+                },
+                "&::after": { border: "none" },
+
+                "& input": {
+                  padding: "0 10px",
+                  width: "280px",
+                  height: "100%",
+                  outline: "none",
+                  borderRadius: "5px",
+                  "&:focus": {
+                    backgroundColor: "white",
+                  },
+                },
+              }}
+            />
+          </Box>
+          <Button
+            type="submit"
+            disableRipple
+            variant="contained"
+            startIcon={<Send />}
+            sx={{
+              backgroundColor: "black",
+              marginTop: "40px",
+              color: "white",
+              width: "110px",
+              height: "50px",
+              "&:hover": { backgroundColor: "black" },
+            }}
+          >
+            Send
+          </Button>
+        </Stack>
+      </Stack>
+    </>
+  );
+}
